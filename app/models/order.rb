af17cea14517d4c7a6fd8673eea399c8bdbdc766
mrcsporto <mrcsporto@gmail.com>
2022-09-25 21:53:05 -0300

    add form validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,19 @@
 class Order < ApplicationRecord
+  validates :referencia_pedido, :email_pedido, :data_pagamento, :nome_cliente, :estado_pedido,
+            :valor_total, :valor_produto, :imagem_produto, :nome_produto, :categoria, :valor_base,
+            :cidade, :estado, :endereco, presence: { message: 'não pode estar em branco' }
+  validates :email_pedido, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'não é um formato válido de e-mail' }
+  validates :valor_total, :valor_produto, :valor_base, numericality: {greater_than: 0 }
+  validates :nome_cliente, :estado_pedido, :nome_produto, :categoria, :cidade, :estado,  
+            length: { minimum: 2, message: "deve conter 3 ou mais letras" }
+  validates :nome_cliente, :estado_pedido, :cidade, :estado, :categoria,
+            format: { with: /\A[\p{Latin}\p{Punct}\p{Space}0-9]*\z/, message: "só é permitido letras" }
+
+
   def self.import(file)
     CSV.foreach(file.path, headers: true, :header_converters => lambda { |h| h.try(:downcase).try(:gsub,' ', '_') }) do |row|
       Order.create! row.to_hash
     end
   end
 end
+
